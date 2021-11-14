d0a6cd47deb24cb520307373b9b2fb777ff45be7
R-YYY <1378012422@qq.com>
2021-11-14 20:00:48 +0800

    上传了src文件夹
    将src文件夹克隆到已有的vue项目中，替换原有的src文件夹即可。后续需要的依赖包在push时提醒一下，大家自己去安装。

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import VueRouter from "vue-router";
+import store from './store'
+
+//+引入组件库及相关样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+//让Vue使用ElementUI
+Vue.use(ElementUI)
+Vue.use(VueRouter)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
