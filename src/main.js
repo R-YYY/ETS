856dbaab766dc8f43543a72e229544e99bcc4c0d
R-YYY <1378012422@qq.com>
2021-11-16 15:50:45 +0800

    配置

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import VueRouter from "vue-router";
 import store from './store'
+import axios from "axios";
 
 //+引入组件库及相关样式
 import ElementUI from 'element-ui'
@@ -10,7 +11,11 @@ import 'element-ui/lib/theme-chalk/index.css'
 
 //让Vue使用ElementUI
 Vue.use(ElementUI)
+
 Vue.use(VueRouter)
+
+Vue.prototype.$axios = axios;
+axios.defaults.baseURL="/api";
 
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,12 @@ import ManageGrade from "@/components/teacherPages/ManageGrade";
 import ManageFile from "@/components/teacherPages/ManageFile";
 import Feedback from "@/components/teacherPages/Feedback";
 
+// 解决重复点击路由报错的BUG
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch((err) => err)
+}
+
 Vue.use(VueRouter)
 
 const routes = [
