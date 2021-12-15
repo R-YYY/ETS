f0f7e7f418130aa3934c79f2ddfa8c94066e3e12
R-YYY <1378012422@qq.com>
2021-12-15 20:04:40 +0800

    批改报告功能

--- src/components/teacherPages/course/ManageCourse.vue
+++ src/components/teacherPages/course/ManageCourse.vue
@@ -91,7 +91,11 @@ export default {
       method: "get",
       params: {
         course_ID: this.$route.params.course_id,
-      }
+      },
+      headers: {
+        token:
+            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxMjM0NTY3In0.rrlord8uupqmlJXvDW6Ha1sGfp5te8ICtSrlaDe1f6o",
+      },
     })
       .then(function (response) {
         _this.courseTeacherID = response.data.teacher_ID;
